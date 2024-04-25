847e050befcecefd26656d4d0e665cf207f6e2f0
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-25 19:01:12 -0300

    Add files via upload

---
+++ pilha/pilha/ItemPilha.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilha
+
+public class ItemPilha();
+{
+    //TAIL
+    int? Cauda; { get; set; }
+
+    //OBJETO
+    object Objeto; { get; set; }
+
+    //READ
+    int? Cabeca; { get; set; }
+
+}
---
+++ pilha/pilha/Program.cs
@@ -0,0 +1,19 @@
+﻿using pilha;
+pilha _pilha = new pilha();
+
+var item = new ItemPilha();
+item.Cauda = null;
+item.Objeto = "teste";
+item.Cabeca = 1;
+
+var item2 = new ItemPilha();
+item2.Cauda = 1;
+item2.Objeto = "jose";
+item2.Cabeca = null;
+
+var item3 = new ItemPilha();
+item3.Cauda = 1;
+item3.Objeto = "jose";
+item3.Cabeca = null;
+
+
